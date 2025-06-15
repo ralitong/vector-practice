6abde881acb5bcb7508244f6a1fdf1f6bc1ddf25
ralitong <ral_itong@yahoo.com>
2025-06-15 13:43:41 +0300

    feat: studied building and managing complex configs

--- notes.md
+++ notes.md
@@ -9,3 +9,6 @@
 - Created a file `vector-05.yaml` that contains an example how to transform logs to be GPDR compliant
 - Created a file `vector-06.yaml` and `vector-07.yaml` that test log namespaces (still in beta mode)
 - Created a file `vector-08.yaml` that sets custom semantics for log events
+- Created a file `vector-09.yaml` to generate template configurations
+- Created a file `vector-10.yaml` to try out unit tests for vector configurations
+- Created a file `vector-11.yaml` to demonstrate separating config files and reading them from a directory
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [[ -z "$vector_available" ]]; then
   exit 1
 fi
 
-function seperate () {
+function separate () {
   echo "-----------------------------------------------------------"
   echo "------------------RUNNING NEW VECTOR-----------------------"
   echo "-----------------------------------------------------------"
@@ -13,25 +13,32 @@ function seperate () {
 
 # (1) Running vector with simple stdin and stdout
 echo "Hello world!" | vector --config ./vector-01.yaml
-seperate
+separate
 # (2) running vector with syslog
 vector --config ./vector-02.yaml
-seperate
+separate
 # (3) running vector with syslog but with more complex remapping
 vector --config ./vector-03.yaml
-seperate
+separate
 # (4) running vector to conform with GPDR laws
 cat <<-EOF | vector --config ./vector-05.toml
   { "id": "user1", "gdpr": false, "email": "us-user1@datadoghq.com" }
   { "id": "user2", "gdpr": false, "email": "us-user2@datadoghq.com" }
   { "id": "user3", "gdpr": true, "email": "eu-user3@datadoghq.com" }
 EOF
-seperate
+separate
 # (5) running vector to test log namespaces
 vector --config ./vector-06.yaml
-seperate
+separate
 # (6) running vector to test log namespaces but with log namespaces disabled
 vector --config ./vector-07.yaml
-seperate
+separate
 # (7) running vector with custom semantics
 vector --config ./vector-08.yaml
+# (8) save time by letting vector generate configs
+vector generate /remap,filter,reduce > vector-09.toml
+separate
+# (9) testing configs
+vector test ./vector-10.toml
+separate
+# (10) 
